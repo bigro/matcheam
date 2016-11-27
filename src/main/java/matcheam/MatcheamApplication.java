9f3c812795d345f5c012de2dfc0c40943fc936f5
大畔祐輝 <ooguro@ooazeyuuakira-no-MacBook-Pro.local>
2016-11-27 16:19:43 +0900

    最初のコミット

---
+++ src/main/java/matcheam/MatcheamApplication.java
@@ -0,0 +1,12 @@
+package matcheam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatcheamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatcheamApplication.class, args);
+	}
+}
---
+++ src/test/java/matcheam/MatcheamApplicationTests.java
@@ -0,0 +1,16 @@
+package matcheam;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MatcheamApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
