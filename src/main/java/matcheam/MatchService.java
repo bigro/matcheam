a9a8ce3ae8753f4a53eec9d0269995067a309756
jnishida <nishida@allora-inc.jp>
2017-01-15 23:13:03 +0900

    #findByLevel(Level) の実装を Stream#filter(〜) に変更

--- src/main/java/matcheam/MatchService.java
+++ src/main/java/matcheam/MatchService.java
@@ -1,8 +1,8 @@
 package matcheam;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -12,26 +12,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class MatchService {
 
-    // TODO 永続化する
-    public HashMap<String, Match> matchMap = new HashMap<>();
+	// TODO 永続化する
+	public HashMap<String, Match> matchMap = new HashMap<>();
 
-    public void register(Match match) {
-        //TODO 永続化する
-        matchMap.put(match.getIdentifier().toString(), match);
+	public void register(Match match) {
+		// TODO 永続化する
+		matchMap.put(match.getIdentifier().toString(), match);
 
-    }
-    
-    public Collection<Match> findAll(){
-    	return matchMap.values();
-    }
-    
-    public Collection<Match> findByLevel(Level level) {
-    	Collection<Match> result = new ArrayList<>();
-		for (Match match : matchMap.values()) {
-			if (level == match.getLevel()) {
-				result.add(match);	
-			}
-		}
-		return result;
+	}
+
+	public Collection<Match> findAll() {
+		return matchMap.values();
+	}
+
+	public Collection<Match> findByLevel(Level level) {
+		return matchMap.values().stream().filter(m -> m.getLevel() == level).collect(Collectors.toList());
 	}
 }
