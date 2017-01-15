1faa0051e599ff38abaaad01f80e6de3101d38f5
jnishida <nishida@allora-inc.jp>
2017-01-15 23:00:23 +0900

    match検索のテストでき太

--- src/main/java/matcheam/Level.java
+++ src/main/java/matcheam/Level.java
@@ -2,5 +2,6 @@ package matcheam;
 
 public enum Level {
 	LEVEL1,
-	LEVEL2;
+	LEVEL2,
+	LEVEL3;
 }
--- src/test/java/matcheam/MatchServiceSearchTest.java
+++ src/test/java/matcheam/MatchServiceSearchTest.java
@@ -1,7 +1,12 @@
 package matcheam;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -11,19 +16,46 @@ public class MatchServiceSearchTest {
 
 	private MatchService matchService = new MatchService();
 
+	@Before
+	public void Before() {
+		matchService.matchMap.clear();
+
+		matchService.matchMap.put("1", createMatch(Level.LEVEL1));
+		matchService.matchMap.put("2", createMatch(Level.LEVEL1));
+		matchService.matchMap.put("3", createMatch(Level.LEVEL3));
+		matchService.matchMap.put("4", createMatch(Level.LEVEL3));
+		matchService.matchMap.put("5", createMatch(Level.LEVEL3));
+	}
+
+	private Match createMatch(Level level) {
+		LocalDateTime date = LocalDateTime.now();
+		Duration gameTime = Duration.ofHours(2);
+		Match match = new Match();
+		match.setIdentifier(new Identifier("1"));
+		match.setPlace("場所");
+		match.setDate(date);
+		match.setGameTime(gameTime);
+		match.setMaxPlayers(10);
+		match.setLevel(level);
+		return match;
+	}
+
 	@Test
 	public void 指定したレベルで絞り混んだ検索ができること() throws Exception {
-
+		Collection<Match> actual = matchService.findByLevel(Level.LEVEL3);
+		assertThat(actual).hasSize(3).extracting(Match::getLevel).containsOnly(Level.LEVEL3);
 	}
 
 	@Test
 	public void 指定したレベルの募集が存在しない場合空のリストが返ってくること() throws Exception {
-
+		Collection<Match> actual = matchService.findByLevel(Level.LEVEL2);
+		assertThat(actual).isEmpty();
 	}
 
 	@Test
 	public void 全件検索できること() throws Exception {
-
+		Collection<Match> actual = matchService.findAll();
+		assertThat(actual).hasSize(5).extracting(Match::getLevel).containsOnly(Level.LEVEL1, Level.LEVEL3);
 	}
 
 }
