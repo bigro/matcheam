cac8eee07595a95fe1a21c6d03e1f22d301daf42
Yuuki Ooguro <kitkat.jaja@gmail.com>
2017-01-21 14:47:53 +0900

    どんな登録情報でも登録完了画面を表示させるようにした

--- src/main/java/matcheam/MatchRegisterController.java
+++ src/main/java/matcheam/MatchRegisterController.java
@@ -1,6 +1,8 @@
 package matcheam;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/match")
 public class MatchRegisterController {
+    @Autowired
+    MatchService matchService;
+
     @ModelAttribute("match")
     Match match() {
         return new Match();
@@ -23,7 +28,8 @@ public class MatchRegisterController {
     }
 
     @PostMapping("result")
-    String register(@ModelAttribute("match") Match match) {
+    String execute(@ModelAttribute("match") Match match, BindingResult bindingResult) {
+        matchService.register(match);
         return "/match/result";
     }
 }
