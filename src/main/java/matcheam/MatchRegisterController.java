466bb6effd3e674d96d3bcac9799866608bc4e1b
Yuuki Ooguro <kitkat.jaja@gmail.com>
2017-01-21 14:34:31 +0900

    登録完了画面を表示させた

--- src/main/java/matcheam/MatchRegisterController.java
+++ src/main/java/matcheam/MatchRegisterController.java
@@ -3,21 +3,27 @@ package matcheam;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by ooguro on 2017/01/21.
  */
 @Controller
-@RequestMapping("/match/register")
+@RequestMapping("/match")
 public class MatchRegisterController {
     @ModelAttribute("match")
     Match match() {
         return new Match();
     }
 
-    @GetMapping
+    @GetMapping("register")
     String show() {
         return "/match/register";
+    }
+
+    @PostMapping("result")
+    String register(@ModelAttribute("match") Match match) {
+        return "/match/result";
     }
 }
