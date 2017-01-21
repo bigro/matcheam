2cb8f085ae3964dc0bd3b257e0c1e9873caee16d
jnishida <nishida@allora-inc.jp>
2017-01-21 15:33:36 +0900

    検索できることになった

--- src/main/java/matcheam/MatchService.java
+++ src/main/java/matcheam/MatchService.java
@@ -1,5 +1,7 @@
 package matcheam;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.stream.Collectors;
@@ -15,6 +17,10 @@ public class MatchService {
 
 	// TODO 永続化する
 	public HashMap<String, Match> matchMap = new HashMap<>();
+
+	public MatchService() {
+		before();
+	}
 
 	public void register(Match match) {
 		// TODO 永続化する
@@ -37,4 +43,27 @@ public class MatchService {
 	private boolean in(Level[] levels, Level level) {
 		return Stream.of(levels).anyMatch(lvls -> lvls == level);
 	}
+
+	private void before() {
+		register(createMatch("1", Level.LEVEL1));
+		register(createMatch("2", Level.LEVEL1));
+		register(createMatch("3", Level.LEVEL3));
+		register(createMatch("4", Level.LEVEL3));
+		register(createMatch("5", Level.LEVEL3));
+		register(createMatch("6", Level.LEVEL4));
+	}
+
+	private Match createMatch(String id, Level level) {
+		LocalDateTime date = LocalDateTime.now();
+		Duration gameTime = Duration.ofHours(2);
+		Match match = new Match();
+		match.setIdentifier(new Identifier(id));
+		match.setPlace("場所");
+		match.setDate(date);
+		match.setGameTime(gameTime);
+		match.setMaxPlayers(10);
+		match.setLevel(level);
+		return match;
+	}
+
 }
