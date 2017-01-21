45f665b23e66661de4f16b14f80663fa92f335cb
jnishida <nishida@allora-inc.jp>
2017-01-21 16:47:01 +0900

    マッチ系コントローラの統合

--- src/main/java/matcheam/MatchController.java
+++ src/main/java/matcheam/MatchController.java
@@ -3,18 +3,40 @@ package matcheam;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
- * 募集検索コントローラ
+ * Created by ooguro on 2017/01/21.
  */
 @Controller
 @RequestMapping("/match")
-public class MatchSearchController {
-	private MatchService matchService = new MatchService();
+public class MatchController {
+    @Autowired
+    MatchService matchService;
+
+    @ModelAttribute("match")
+    Match match() {
+        return new Match();
+    }
+
+    @GetMapping("register")
+    String show() {
+        return "/match/register";
+    }
+
+    @PostMapping("result")
+    String execute(@ModelAttribute("match") Match match, BindingResult bindingResult) {
+        matchService.register(match);
+        return "/match/result";
+    }
 
 	/**
 	 * @param model
