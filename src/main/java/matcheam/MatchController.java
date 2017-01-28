9804d05e736a335928aaeeeedaeee4db4832fbf3
Yuuki Ooguro <kitkat.jaja@gmail.com>
2017-01-28 16:19:32 +0900

    フォーマットかけた

--- src/main/java/matcheam/MatchController.java
+++ src/main/java/matcheam/MatchController.java
@@ -1,17 +1,13 @@
 package matcheam;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * Created by ooguro on 2017/01/21.
@@ -38,29 +34,27 @@ public class MatchController {
         return "/match/result";
     }
 
-	/**
-	 * @param model
-	 *            テンプレートが表示するときに使う情報の設定先
-	 * @param level
-	 *            レベル
-	 * @return 表示するテンプレート
-	 */
-	@RequestMapping(value = "/search")
-	public String search(Model model, @RequestParam(required = false) String level) {
-		if (level == null || level.isEmpty()) {
-			Collection<Match> matches = matchService.findAll();
-			model.addAttribute("level", "");
-			model.addAttribute("matches", matches);
-			return "match/search";
-		}
-		try {
-			Collection<Match> matches = matchService.findByLevel(Level.valueOf(level));
-			model.addAttribute("level", level);
-			model.addAttribute("matches", matches);
-		} catch (IllegalArgumentException e) {
-			model.addAttribute("level", level);
-			model.addAttribute("matches", new ArrayList<Match>());
-		}
-		return "match/search";
-	}
+    /**
+     * @param model テンプレートが表示するときに使う情報の設定先
+     * @param level レベル
+     * @return 表示するテンプレート
+     */
+    @RequestMapping(value = "/search")
+    public String search(Model model, @RequestParam(required = false) String level) {
+        if (level == null || level.isEmpty()) {
+            Collection<Match> matches = matchService.findAll();
+            model.addAttribute("level", "");
+            model.addAttribute("matches", matches);
+            return "match/search";
+        }
+        try {
+            Collection<Match> matches = matchService.findByLevel(Level.valueOf(level));
+            model.addAttribute("level", level);
+            model.addAttribute("matches", matches);
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("level", level);
+            model.addAttribute("matches", new ArrayList<Match>());
+        }
+        return "match/search";
+    }
 }
