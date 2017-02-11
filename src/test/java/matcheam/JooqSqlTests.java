be51e9fb66fa89a7814f04ee65a3df72d5e86224
Yuuki Ooguro <kitkat.jaja@gmail.com>
2017-02-11 15:55:43 +0900

    jooqでselectできたよー

--- src/test/java/matcheam/JooqSqlTests.java
+++ src/test/java/matcheam/JooqSqlTests.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-import static org.jooq.impl.DSL.table;
+import static nu.studer.sample.tables.Author.AUTHOR;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -21,13 +21,20 @@ public class JooqSqlTests {
 
     @Test
     public void insertSelect() {
-        String userName = "sa";
+        String userName = "root";
         String password = "";
-        String url = "jdbc:h2:~/testdb";
+        String url = "jdbc:mysql://localhost:3306/library";
 
         try (Connection conn = DriverManager.getConnection(url, userName, password)) {
             DSLContext create = DSL.using(conn, SQLDialect.MYSQL);
-            Result<Record> result = create.select().from(table("matcheam.match")).fetch();
+            Result<Record> result = create.select().from(AUTHOR).fetch();
+            for (Record r : result) {
+                Integer id = r.getValue(AUTHOR.ID);
+                String firstName = r.getValue(AUTHOR.FIRST_NAME);
+                String lastName = r.getValue(AUTHOR.LAST_NAME);
+
+                System.out.println("ID: " + id + " first name: " + firstName + " last name: " + lastName);
+            }
         }
         // For the sake of this tutorial, let's keep exception handling simple
         catch (Exception e) {
