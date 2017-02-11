52a5c784feb1f3c2b5385e140bcc2ffbb60cf382
Yuuki Ooguro <kitkat.jaja@gmail.com>
2017-02-11 17:33:41 +0900

    変なインデント修正

--- src/test/java/matcheam/MatcheamApplicationTests.java
+++ src/test/java/matcheam/MatcheamApplicationTests.java
@@ -10,9 +10,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class MatcheamApplicationTests {
 
-	@Test
+    @Test
     @Ignore
-	public void contextLoads() {
-	}
+    public void contextLoads() {
+    }
 
 }
