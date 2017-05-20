502d864808059c7c6f1176d8bdba9fbf418d3477
大畔祐輝 <kitkat.jaja@gmail.com>
2017-05-20 18:01:49 +0900

    画面側修正

--- src/main/java/matcheam/match/MatchService.java
+++ src/main/java/matcheam/match/MatchService.java
@@ -1,17 +1,14 @@
 package matcheam.match;
 
+import matcheam.common.SystemContext;
 import matcheam.common.exception.SystemException;
 import org.jooq.DSLContext;
 import org.jooq.Record;
 import org.jooq.Result;
-import org.jooq.SQLDialect;
-import org.jooq.impl.DSL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -80,12 +77,7 @@ public class MatchService {
 	}
 
 	public Collection<Match> findAll() {
-		String userName = "sa";
-		String password = "";
-		String url = "jdbc:h2:file:./.data/h2/db;";
-
-		try (Connection conn = DriverManager.getConnection(url, userName, password)) {
-			DSLContext create = DSL.using(conn, SQLDialect.MYSQL);
+		try (DSLContext create = new SystemContext().dslContext()) {
 			Result<Record> records = create.select().from(MATCH).fetch();
 			List<Match> matches = new ArrayList<>();
 			for (Record record : records) {
