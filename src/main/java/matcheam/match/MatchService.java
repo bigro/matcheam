36baea63bd0f6d6fc085974c3c3ccb1283ecb6ed
Yuuki Ooguro <kitkat.jaja@gmail.com>
2017-06-03 16:41:12 +0900

    フォーマッター適用

--- src/main/java/matcheam/match/MatchService.java
+++ src/main/java/matcheam/match/MatchService.java
@@ -28,104 +28,104 @@ import matcheam.common.exception.SystemException;
 @Service
 public class MatchService {
 
-    /**
-     * matchのリポジトリ
-     */
-    MatchRepository repository;
+	/**
+	 * matchのリポジトリ
+	 */
+	MatchRepository repository;
 
-    @Autowired
-    DSLContext dsl;
+	@Autowired
+	DSLContext dsl;
 
-    // TODO 永続化するまでの仮置き場
-    public HashMap<String, Match> matchMap = new HashMap<>();
+	// TODO 永続化するまでの仮置き場
+	public HashMap<String, Match> matchMap = new HashMap<>();
 
-    /**
-     * コンストラクタです。
-     */
-    public MatchService() {
-        before();
-    }
+	/**
+	 * コンストラクタです。
+	 */
+	public MatchService() {
+		before();
+	}
 
-    /**
-     * コンストラクタです。
-     *
-     * @param repository matchのリポジトリ
-     */
-    @Autowired
-    public MatchService(MatchRepository repository) {
-        this.repository = repository;
-    }
+	/**
+	 * コンストラクタです。
+	 *
+	 * @param repository matchのリポジトリ
+	 */
+	@Autowired
+	public MatchService(MatchRepository repository) {
+		this.repository = repository;
+	}
 
-    /**
-     * １件のmatchを登録します。
-     *
-     * @param match 募集
-     * @return 登録されたレコードの{@link Identifier}のインスタンス
-     */
-    public Identifier register(Match match) {
-        try {
-            return repository.register(match);
-        } catch (Exception e) {
-            throw new SystemException(e);
-        }
-    }
+	/**
+	 * １件のmatchを登録します。
+	 *
+	 * @param match 募集
+	 * @return 登録されたレコードの{@link Identifier}のインスタンス
+	 */
+	public Identifier register(Match match) {
+		try {
+			return repository.register(match);
+		} catch (Exception e) {
+			throw new SystemException(e);
+		}
+	}
 
-    /**
-     * 主キーで検索します。
-     *
-     * @param identifier 募集の主キー
-     * @return 募集
-     */
-    public Match findBy(Identifier identifier) {
-        try {
-            return repository.findBy(identifier);
-        } catch (Exception e) {
-            throw new SystemException(e);
-        }
-    }
+	/**
+	 * 主キーで検索します。
+	 *
+	 * @param identifier 募集の主キー
+	 * @return 募集
+	 */
+	public Match findBy(Identifier identifier) {
+		try {
+			return repository.findBy(identifier);
+		} catch (Exception e) {
+			throw new SystemException(e);
+		}
+	}
 
-    public Collection<Match> findAll() {
-        Result<Record> records = dsl.select().from(MATCH).fetch();
-        List<Match> matches = new ArrayList<>();
-        for (Record record : records) {
-            Match match = new Match();
-            match.setIdentifier(new Identifier(record.get(MATCH.IDENTIFIER)));
-            match.setDate(record.get(MATCH.DATE));
-            match.setTime(record.get(MATCH.TIME));
-            match.setPlace(record.get(MATCH.PLACE));
-            match.setMaxPlayers(record.get(MATCH.MAXPLAYERS));
-            match.setLevel(Level.valueOf(record.get(MATCH.LEVEL)));
-            matches.add(match);
-        }
-        return matches;
-    }
+	public Collection<Match> findAll() {
+		Result<Record> records = dsl.select().from(MATCH).fetch();
+		List<Match> matches = new ArrayList<>();
+		for (Record record : records) {
+			Match match = new Match();
+			match.setIdentifier(new Identifier(record.get(MATCH.IDENTIFIER)));
+			match.setDate(record.get(MATCH.DATE));
+			match.setTime(record.get(MATCH.TIME));
+			match.setPlace(record.get(MATCH.PLACE));
+			match.setMaxPlayers(record.get(MATCH.MAXPLAYERS));
+			match.setLevel(Level.valueOf(record.get(MATCH.LEVEL)));
+			matches.add(match);
+		}
+		return matches;
+	}
 
-    public Collection<Match> findByLevel(Level... levels) {
-        return findAll().stream().filter(m -> in(levels, m.getLevel())).collect(Collectors.toList());
-    }
+	public Collection<Match> findByLevel(Level... levels) {
+		return findAll().stream().filter(m -> in(levels, m.getLevel())).collect(Collectors.toList());
+	}
 
-    private boolean in(Level[] levels, Level level) {
-        return Stream.of(levels).anyMatch(lvls -> lvls == level);
-    }
+	private boolean in(Level[] levels, Level level) {
+		return Stream.of(levels).anyMatch(lvls -> lvls == level);
+	}
 
-    private void before() {
-        register(createMatch("1", Level.LEVEL1));
-        register(createMatch("2", Level.LEVEL1));
-        register(createMatch("3", Level.LEVEL3));
-        register(createMatch("4", Level.LEVEL3));
-        register(createMatch("5", Level.LEVEL3));
-        register(createMatch("6", Level.LEVEL4));
-    }
+	private void before() {
+		register(createMatch("1", Level.LEVEL1));
+		register(createMatch("2", Level.LEVEL1));
+		register(createMatch("3", Level.LEVEL3));
+		register(createMatch("4", Level.LEVEL3));
+		register(createMatch("5", Level.LEVEL3));
+		register(createMatch("6", Level.LEVEL4));
+	}
 
-    private Match createMatch(String id, Level level) {
-        LocalDate date = LocalDate.now();
-        Match match = new Match();
-        match.setIdentifier(new Identifier(id));
-        match.setPlace("場所");
-        match.setDate(date);
-        match.setTime("2時間");
-        match.setMaxPlayers(BigDecimal.TEN);
-        match.setLevel(level);
-        return match;
-    }
+	private Match createMatch(String id, Level level) {
+		LocalDate date = LocalDate.now();
+		Match match = new Match();
+		match.setIdentifier(new Identifier(id));
+		match.setPlace("場所");
+		match.setDate(date);
+		match.setTime("2時間");
+		match.setMaxPlayers(BigDecimal.TEN);
+		match.setLevel(level);
+		return match;
+	}
 }
