ebf58dfda3038776b5787a4bd77682d30367a96a
Yuuki Ooguro <kitkat.jaja@gmail.com>
2017-07-08 17:24:04 +0900

    findAllのテストの完了

--- src/test/java/matcheam/match/MatchRepositoryTest.java
+++ src/test/java/matcheam/match/MatchRepositoryTest.java
@@ -7,9 +7,12 @@ import matcheam.support.TestContext;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static com.ninja_squad.dbsetup.Operations.deleteAllFrom;
+import static com.ninja_squad.dbsetup.Operations.insertInto;
+import static com.ninja_squad.dbsetup.Operations.sequenceOf;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
 
@@ -19,10 +22,11 @@ import static org.junit.Assert.*;
 public class MatchRepositoryTest {
 
 	private MatchRepository matchRepository;
+	private TestContext testContext;
 
 	@Before
 	public void setUp() throws Exception {
-		TestContext testContext = new TestContext();
+		testContext = new TestContext();
 		matchRepository = new MatchRepository(testContext.dslContext());
 		Operation deleteAll = deleteAllFrom("MATCHEAM.MATCH");
 		DbSetup dbSetup = new DbSetup(testContext.driverManagerDestination(), deleteAll);
@@ -31,8 +35,17 @@ public class MatchRepositoryTest {
 
 	@Test
 	public void 全件検索できること() throws Exception {
+		Operation operation =
+			insertInto("MATCHEAM.MATCH")
+				.columns("place", "date", "start", "time", "level", "maxPlayers")
+				.values("尼崎", LocalDate.of(2017, 7, 7), "12時", "2時間", Level.LEVEL1, 12)
+				.values("神戸", LocalDate.of(2017, 7, 7), "12時", "2時間", Level.LEVEL1, 12)
+				.values("フランス", LocalDate.of(2017, 7, 7), "12時", "2時間", Level.LEVEL1, 12)
+				.build();
+		DbSetup dbSetup = new DbSetup(testContext.driverManagerDestination(), operation);
+		dbSetup.launch();
 		List<Match> actual = matchRepository.findAll();
-		assertThat(actual).isNotEmpty();
+		assertThat(actual).hasSize(3);
 	}
 }
 
