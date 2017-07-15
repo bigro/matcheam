aeb4d11f713c4552b0c732c1f468d3beba716477
Yuuki Ooguro <kitkat.jaja@gmail.com>
2017-07-15 16:38:07 +0900

    MatchRepositorySearchTestのデータ用意を共通化

--- src/test/java/matcheam/match/MatchRepositorySearchTest.java
+++ src/test/java/matcheam/match/MatchRepositorySearchTest.java
@@ -1,14 +1,11 @@
 package matcheam.match;
 
 import com.ninja_squad.dbsetup.DbSetup;
-import com.ninja_squad.dbsetup.operation.Insert;
 import com.ninja_squad.dbsetup.operation.Operation;
 import matcheam.support.TestContext;
-import org.assertj.core.api.SoftAssertions;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -28,65 +25,42 @@ public class MatchRepositorySearchTest {
 	@Before
 	public void setUp() throws Exception {
 		testContext = new TestContext();
-		matchRepository = new MatchRepository(testContext.dslContext());
-		Operation deleteAll = deleteAllFrom("MATCHEAM.MATCH");
-		DbSetup dbSetup = new DbSetup(testContext.driverManagerDestination(), deleteAll);
+		Operation operation =
+			sequenceOf(
+				deleteAllFrom("MATCHEAM.MATCH"),
+				insertInto("MATCHEAM.MATCH")
+					.columns("identifier", "place", "date", "start", "time", "level", "maxPlayers")
+					.values(1, "尼崎", LocalDate.of(2017, 7, 7), "12時", "2時間", Level.LEVEL1, 12)
+					.values(2, "神戸", LocalDate.of(2017, 7, 7), "12時", "2時間", Level.LEVEL3, 12)
+					.values(3, "フランス", LocalDate.of(2017, 7, 7), "12時", "2時間", Level.LEVEL4, 12)
+					.build()
+			);
+		DbSetup dbSetup = new DbSetup(testContext.driverManagerDestination(), operation);
 		dbSetup.launch();
+
+		matchRepository = new MatchRepository(testContext.dslContext());
 	}
 
 	@Test
 	public void 全件検索できること() throws Exception {
-		Operation operation =
-			insertIntoMatcheam()
-				.values("尼崎", LocalDate.of(2017, 7, 7), "12時", "2時間", Level.LEVEL1, 12)
-				.values("神戸", LocalDate.of(2017, 7, 7), "12時", "2時間", Level.LEVEL1, 12)
-				.values("フランス", LocalDate.of(2017, 7, 7), "12時", "2時間", Level.LEVEL1, 12)
-				.build();
-		DbSetup dbSetup = new DbSetup(testContext.driverManagerDestination(), operation);
-		dbSetup.launch();
 		List<Match> actual = matchRepository.findAll();
 		assertThat(actual).hasSize(3);
 	}
 
 	@Test
 	public void 指定したレベル1つで絞り混んだ検索ができること() throws Exception {
-		Operation operation =
-			insertIntoMatcheam()
-				.values("尼崎", LocalDate.of(2017, 7, 7), "12時", "2時間", Level.LEVEL1, 12)
-				.values("神戸", LocalDate.of(2017, 7, 7), "12時", "2時間", Level.LEVEL3, 12)
-				.values("フランス", LocalDate.of(2017, 7, 7), "12時", "2時間", Level.LEVEL4, 12)
-				.build();
-		DbSetup dbSetup = new DbSetup(testContext.driverManagerDestination(), operation);
-		dbSetup.launch();
-
 		List<Match> actual = matchRepository.findBy(Level.LEVEL3);
 		assertThat(actual).extracting(Match::getLevel).containsOnly(Level.LEVEL3);
 	}
 
 	@Test
 	public void 指定したレベルの募集が存在しない場合空のリストが返ってくること() throws Exception {
-		Operation operation =
-			insertIntoMatcheam()
-				.values("尼崎", LocalDate.of(2017, 7, 7), "12時", "2時間", Level.LEVEL1, 12)
-				.values("神戸", LocalDate.of(2017, 7, 7), "12時", "2時間", Level.LEVEL3, 12)
-				.values("フランス", LocalDate.of(2017, 7, 7), "12時", "2時間", Level.LEVEL4, 12)
-				.build();
-		DbSetup dbSetup = new DbSetup(testContext.driverManagerDestination(), operation);
-		dbSetup.launch();
 		List<Match> actual = matchRepository.findBy(Level.LEVEL2);
 		assertThat(actual).isEmpty();
 	}
 
 	@Test
 	public void 指定したIDで絞り混んだ検索ができること() throws Exception {
-		Operation operation =
-			insertInto("MATCHEAM.MATCH")
-				.columns("identifier", "place", "date", "start", "time", "level", "maxPlayers")
-				.values(1, "尼崎", LocalDate.of(2017, 7, 7), "12時", "2時間", Level.LEVEL1, 12)
-				.build();
-		DbSetup dbSetup = new DbSetup(testContext.driverManagerDestination(), operation);
-		dbSetup.launch();
-
 		Match actual = matchRepository.findBy(new Identifier("1"));
 		assertThat(actual).isNotNull();
 	}
@@ -96,11 +70,4 @@ public class MatchRepositorySearchTest {
 		Match actual = matchRepository.findBy(new Identifier("X"));
 		assertThat(actual).isNull();
 	}
-
-	private Insert.Builder insertIntoMatcheam() {
-		return insertInto("MATCHEAM.MATCH")
-			.columns("place", "date", "start", "time", "level", "maxPlayers");
-	}
 }
-
-
