f0fd5dbdb0feaf8e9f4af9b320c02a30abc251ee
jnishida <nishida@allora-inc.jp>
2017-08-05 14:54:31 +0900

    応募と応募者のテーブルを作った

--- src/main/resources/db/migration/V1__Create_match_table.sql
+++ src/main/resources/db/migration/V1__Create_match_table.sql
@@ -1,5 +1,8 @@
 CREATE SCHEMA IF NOT EXISTS matcheam;
 
+/*
+ * match 募集
+ */
 CREATE TABLE matcheam.match (
 identifier VARCHAR(100) PRIMARY KEY AUTO_INCREMENT,
 place VARCHAR(100),
@@ -8,4 +11,21 @@ start VARCHAR(10),
 time VARCHAR(10),
 level VARCHAR(30),
 maxPlayers DECIMAL(3)
+);
+
+/*
+ * entry 応募
+ */
+CREATE TABLE matcheam.entry (
+identifier VARCHAR(100) PRIMARY KEY AUTO_INCREMENT,
+matchId VARCHAR(100) not null,
+entryUserId VARCHAR(100) not null
+);
+
+/*
+ * entryUser 応募者
+ */
+CREATE TABLE matcheam.entryUser (
+identifier VARCHAR(100) PRIMARY KEY AUTO_INCREMENT,
+entryUserName VARCHAR(20)
 );
