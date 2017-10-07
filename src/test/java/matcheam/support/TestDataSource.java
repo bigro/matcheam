baea7b7a41c3c438f8f1c5954eb09628904a2f78
Yuuki Ooguro <kitkat.jaja@gmail.com>
2017-10-07 15:20:37 +0900

    テスト用のDB情報をプロパティから取得

--- src/test/java/matcheam/support/TestDataSource.java
+++ src/test/java/matcheam/support/TestDataSource.java
@@ -1,8 +1,10 @@
 package matcheam.support;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import com.ninja_squad.dbsetup.destination.DriverManagerDestination;
 import org.jooq.DSLContext;
@@ -10,40 +12,48 @@ import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
 
 public class TestDataSource implements AutoCloseable {
-	private static final String TEST_DB_URL = "jdbc:mysql://localhost:3306/matcheam";
-	private static final String TEST_DB_USER = "matcheam";
-	private static final String TEST_DB_PASSWORD = "pass";
-	private Connection connection;
-	private DSLContext dslContext;
+    private String testDbUrl;
+    private String testDbUser;
+    private String testDbPassword;
+    private Connection connection;
+    private DSLContext dslContext;
 
-	public Connection connection() {
-		return connection;
-	}
+    public Connection connection() {
+        return connection;
+    }
 
-	public DSLContext dslContext() {
-		return dslContext;
-	}
+    public DSLContext dslContext() {
+        return dslContext;
+    }
 
-	public DriverManagerDestination driverManagerDestination() throws SQLException {
-		return new DriverManagerDestination(TEST_DB_URL, TEST_DB_USER, TEST_DB_PASSWORD);
-	}
+    public DriverManagerDestination driverManagerDestination() throws SQLException {
+        return new DriverManagerDestination(testDbUrl, testDbUser, testDbPassword);
+    }
 
-	public TestDataSource() throws SQLException {
-		this.connection = getConnect();
-		this.dslContext = getDSLContext(connection);
-	}
+    public TestDataSource() throws Exception {
+        ClassLoader loader = ClassLoader.getSystemClassLoader();
+        try (InputStream stream = loader.getResourceAsStream("application-local.properties")) {
+            Properties properties = new Properties();
+            properties.load(stream);
+            testDbUrl = properties.getProperty("spring.datasource.url");
+            testDbUser = properties.getProperty("spring.datasource.username");
+            testDbPassword = properties.getProperty("spring.datasource.password");
+        }
+        this.connection = getConnect();
+        this.dslContext = getDSLContext(connection);
+    }
 
-	private Connection getConnect() throws SQLException {
-		return DriverManager.getConnection(TEST_DB_URL, TEST_DB_USER, TEST_DB_PASSWORD);
-	}
+    private Connection getConnect() throws SQLException {
+        return DriverManager.getConnection(testDbUrl, testDbUser, testDbPassword);
+    }
 
-	private DSLContext getDSLContext(Connection conn) {
-		return DSL.using(conn, SQLDialect.MYSQL);
-	}
+    private DSLContext getDSLContext(Connection conn) {
+        return DSL.using(conn, SQLDialect.MYSQL);
+    }
 
-	@Override
-	public void close() throws Exception {
-		dslContext.close();
-		connection.close();
-	}
+    @Override
+    public void close() throws Exception {
+        dslContext.close();
+        connection.close();
+    }
 }
