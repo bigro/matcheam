6544a141da2e672f051a67f48bfd4ac78243feb6
jnishida <nishida@allora-inc.jp>
2018-02-10 16:59:38 +0900

    試合応募人数を表示できるようにした

--- src/main/java/matcheam/match/Match.java
+++ src/main/java/matcheam/match/Match.java
@@ -108,4 +108,8 @@ public class Match {
     public void setEntryUserList(List<EntryUser> entryUserList) {
         this.entryUserList = entryUserList;
     }
+   
+    public int getEntryUserCount(){
+    	return this.entryUserList.size();
+    }
 }
